93d2a3560828f51fa0de1bb47ce52b65f1eb5dca
rizfauzi <rejekifauzi@gmail.com>
2024-08-25 00:48:10 +0700

    Deploy website - based on

--- assets/js/361f119e.bbea9c95.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfauziweb=self.webpackChunkfauziweb||[]).push([[6553],{2845:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/fauziweb/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.b069397b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfauziweb=self.webpackChunkfauziweb||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/fauziweb/blog","blogTitle":"Blog","authorsListPath":"/fauziweb/blog/authors"}')}}]);
